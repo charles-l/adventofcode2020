82345a21506af44769a6a6aaa9c0aa8da2c9a01b
nc <charles.saternos@gmail.com>
2020-12-25 21:59:06 -0500

    optimize day23_part2.py

--- day23_part2.py
+++ day23_part2.py
@@ -1,47 +1,40 @@
-# for part two, I realized
-# realized the general problem was actually better with linked lists and
-# a fast hash table lookup and rewrote it entirely.
-import time
 import array
-from llist import dllist, dllistnode
 
-input_str = '942387615'
-def circular_next(dllist, node):
-    if not node.next:
-        return dllist.first
-    else:
-        return node.next
+# 0 is unused (hence the + 1)
+upper_bound = (1000000 + 1)
+next_arr = array.array('I', [0] * upper_bound)
+input_list = list(map(int, '942387615'))
 
-def remove_node(lst, node):
-    lst.remove(node)
-    return node
+# tricky setup code... figure out how to simplify
+for i in range(len(input_list)-1):
+    next_arr[input_list[i]] = input_list[i+1]
+next_arr[input_list[-1]] = 10
+for i in range(10, 1000000):
+    next_arr[i] = i+1
+next_arr[-1] = 9
+min_cup, max_cup = 1, 1000000
 
-cups = dllist([int(x) for x in input_str] + list(range(10, 1000000+1)))
+def remove_next(n):
+    i = next_arr[n]
+    next_arr[n] = next_arr[next_arr[n]]
+    return i
+
+def insert_next(node, n):
+    next_arr[n] = next_arr[node]
+    next_arr[node] = n
+
+current_cup = 9
 moves = 10000000
 
-lookup = {}
-x = cups.first
-while x:
-    lookup[x.value] = x
-    x = x.next
-
-min_cup = min(lookup.keys())
-max_cup = max(lookup.keys())
-
-current_cup = cups.first
 for m in range(moves):
-    picked_up = [remove_node(cups, circular_next(cups, current_cup)) for _ in range(3)]
+    picked_up = [remove_next(current_cup) for _ in range(3)]
+    dest_cup = current_cup - 1
+    while dest_cup in picked_up or dest_cup < min_cup:
+        dest_cup -= 1
+        if dest_cup < min_cup:
+            dest_cup = max_cup
 
-    destination_cup = current_cup.value - 1
-    while destination_cup in (x.value for x in picked_up) or destination_cup < min_cup:
-        destination_cup -= 1
-        if destination_cup < min_cup:
-            destination_cup = max_cup
-
-    destination_cup_node = lookup[destination_cup]
     for x in reversed(picked_up):
-        cups.insertnode(x, circular_next(cups, destination_cup_node))
-
-    current_cup = circular_next(cups, current_cup)
-
-print(circular_next(cups, lookup[1]).value * circular_next(cups, circular_next(cups, lookup[1])).value)
+        insert_next(dest_cup, x)
+    current_cup = next_arr[current_cup]
+print(next_arr[1] * next_arr[next_arr[1]])
