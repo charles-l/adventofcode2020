41e9fcf5133fe193fd5329287b3d49d5e3ea6de8
nc <charles.saternos@gmail.com>
2020-12-26 11:44:41 -0500

    optimize day23_part2.pyx

--- day23_part2.pyx
+++ day23_part2.pyx
@@ -1,32 +1,32 @@
 # cython: language_level=3
+from cpython cimport array
+import itertools
 import array
 
 # 0 is unused (hence the + 1)
 upper_bound = (1000000 + 1)
-next_arr = array.array('I', [0] * upper_bound)
+cdef array.array next_arr = array.array('I', [0] * upper_bound)
 input_list = list(map(int, '942387615'))
 
-# tricky setup code... figure out how to simplify
-for i in range(len(input_list)-1):
-    next_arr[input_list[i]] = input_list[i+1]
-next_arr[input_list[-1]] = 10
-for i in range(10, 1000000):
-    next_arr[i] = i+1
+for a, b in zip(itertools.chain(input_list, range(10, upper_bound)),
+                itertools.chain(input_list[1:], range(10, upper_bound))):
+    next_arr[a] = b
 next_arr[-1] = 9
+
 min_cup, max_cup = 1, 1000000
 
-def remove_next(n):
+cdef int remove_next(int n):
     i = next_arr[n]
     next_arr[n] = next_arr[next_arr[n]]
     return i
 
-def insert_next(node, n):
+cdef int insert_next(int node, int n):
     next_arr[n] = next_arr[node]
     next_arr[node] = n
 
 def do_computation():
-    current_cup = 9
-    moves = 10000000
+    cdef int current_cup = 9
+    cdef int moves = 10000000
 
     for m in range(moves):
         picked_up = [remove_next(current_cup) for _ in range(3)]
